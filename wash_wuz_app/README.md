8b304dfd603ad3a0b70c9cb57d98538c7257bd3f
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-01-09 11:48:02 +0700

    initialize flutter

---
+++ wash_wuz_app/README.md
@@ -0,0 +1,3 @@
+# wash_wuz_app
+
+A new Flutter project.
---
+++ wash_wuz_app/android/app/src/main/kotlin/com/example/wash_wuz_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wash_wuz_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
